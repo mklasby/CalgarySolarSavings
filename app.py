ccd90b1cdb5a4c1232840c0f2284cbc67bc1fd33
Stan Chen <stanleychen587@gmail.com>
2021-02-13 15:12:09 -0700

    added default tempalte

--- app.py
+++ app.py
@@ -1,42 +1,88 @@
-from flask import Flask, render_template
-from flask_googlemaps import GoogleMaps
-from flask_googlemaps import Map
-
-app = Flask(__name__, template_folder=".")
-GoogleMaps(app)
-app.config['GOOGLEMAPS_KEY'] = "AIzaSyC-8AGexW-2EbYPrMbQxehC0ZrfwIkrmLg"
-
-@app.route("/")
-def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)],
-        styles="height:300px;width:300px;margin:0;"
+"""
+plot documentation: https://dash.plot.ly/urls
+"""
+import datetime
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+time_display = datetime.date.today()
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+sidebar = html.Div(
+    [
+        html.H2("[Title:PH]", className="display-4"),
+        html.Hr(),
+        html.P(
+            "[Group:PH] \n "+str(time_display), className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Search Address", href="/page-1", id="page-1-link"),
+                dbc.NavLink("Saving: Solar Sun Roof", href="/page-2", id="page-2-link"),
+                dbc.NavLink("Install Solar", href="/page-3", id="page-3-link"),
+                dbc.NavLink("Contact Contractors", href="/page-4", id="page-4-link"),
+                dbc.NavLink("Secure Loan", href="/page-5", id="page-5-link"),
+                dbc.NavLink("Initiatives", href="/page-6", id="page-6-link"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+# this callback uses the current pathname to set the active state of the
+# corresponding nav link to true, allowing users to tell see page they are on
+@app.callback(
+    [Output(f"page-{i}-link", "active") for i in range(1, 7)],
+    [Input("url", "pathname")],
+)
+def toggle_active_links(pathname):
+    if pathname == "/":
+        # Treat page 1 as the homepage / index
+        return True, False, False
+    return [pathname == f"/page-{i}" for i in range(1, 7)]
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/page-1"]:
+        return html.P("[PH]")
+    elif pathname == "/page-2":
+        return html.P("[PH]")
+    elif pathname == "/page-3":
+        return html.P("[PH]")
+    elif pathname == "/page-4":
+        return html.P("[PH]")
+    elif pathname == "/page-5":
+        return html.P("[PH]")
+    elif pathname == "/page-6":
+        return html.P("[PH]")
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
     )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ],
-        styles="height:300px;width:300px;margin:0;"
-    )
-    return render_template('example.html', mymap=mymap, sndmap=sndmap)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run_server(port=8888)
