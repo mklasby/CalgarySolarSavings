0b43db746b7e1187c31ad18074248d364101bb3e
Stan Chen <stanleychen587@gmail.com>
2021-02-13 22:14:09 -0700

    updated app.py

--- app.py
+++ app.py
@@ -8,8 +8,23 @@ app = Flask(__name__, static_url_path='/static', template_folder='static', )
 
 # a route where we will display a welcome message via an HTML template
 @app.route("/")
+@app.route("/index.html")
 def home():
     return render_template("index.html")
+
+@app.route("/avalibility.html")
+def availability():
+    return render_template("avalibility.html")
+
+@app.route("/ownership.html")
+def owner():
+    return render_template("ownership.html")
+
+@app.route("/page3.html")
+def page3():
+    return render_template("page3.html")
+
+
 
 
 # run the application
