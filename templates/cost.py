2f28966aff536f35b93a543c5dd85ba76a94aadb
Tong Xu <jennytong.xu@gamil.com>
2021-02-14 08:07:20 -0700

    edit cost.py, create static component for info

--- templates/cost.py
+++ templates/cost.py
@@ -28,7 +28,7 @@ app.layout = html.Div([
 )
 def output_energy_gen(input_value):
     if input_value != 0:
-        return 'The average installation cost of a {:.0f} kW system in Alberta is between ${:.2f} and ${:.2f}. You can expect about {:.0f} kWh of energy production from this system.'.format(input_value, input_value*1000*2.51, input_value*1000*2.77, input_value*1276)
+        return 'The average installation cost of a {:.0f} kW system in Alberta is between ${:.2f} and ${:.2f}. You can expect about {:.0f} kWh of energy production from this system. You can earn back you investment in {:.1f} to {:.1f} years assuming an average electricity cost of $0.167/kWh, without considering incentives.'.format(input_value, input_value*1000*2.51, input_value*1000*2.77, input_value*1276, input_value*1000*2.51/(1276*0.167*input_value), input_value*1000*2.77/(1276*0.167*input_value))
 
 if __name__ == '__main__':
     app.run_server(debug=True)
